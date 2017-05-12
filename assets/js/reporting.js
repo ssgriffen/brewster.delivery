7ce875b888a7479369e64c2f3db11b7aba45cd08
Sam Griffen <samsgriffen@gmail.com>
2017-05-12 08:12:52 -0700

    JS for reporting

---
+++ assets/js/reporting.js
@@ -0,0 +1,467 @@
+var db = [
+  {
+    "Date": "04-08-2017",
+    "Total Sales (without service fee)": 29,
+    "Service Fees": 3.75,
+    "Total Orders": 3,
+    "Products Sold": 5,
+    "Water": 0,
+    "Coca-Cola": 1,
+    "Labatt Blue Light": 0,
+    "Labatt Blue": 0,
+    "Coors Light": 1,
+    "Blue Moon": 0,
+    "Labatt Blue Light Lime": 1,
+    "Diet Coke": 0,
+    "Root Beer": 0,
+    "Sprite": 0,
+    "Lemonade": 0,
+    "Pretzel": 0,
+    "Pretzel w/ Cheese": 0,
+    "Hot Dog": 1,
+    "Nachos w/ Cheese": 0,
+    "Cracker Jack": 0,
+    "Tenders & Fries": 1,
+    "M&M's": 0,
+    "M&M Peanut": 0,
+    "Peanuts": 0,
+    "Dicso Lemonade": 0
+  },
+  {
+    "Date": "04-09-2017",
+    "Total Sales (without service fee)": 141.25,
+    "Service Fees": 20.25,
+    "Total Orders": 12,
+    "Products Sold": 27,
+    "Water": 1,
+    "Coca-Cola": 0,
+    "Labatt Blue Light": 10,
+    "Labatt Blue": 4,
+    "Coors Light": 1,
+    "Blue Moon": 0,
+    "Labatt Blue Light Lime": 1,
+    "Diet Coke": 0,
+    "Root Beer": 1,
+    "Sprite": 1,
+    "Lemonade": 0,
+    "Pretzel": 0,
+    "Pretzel w/ Cheese": 0,
+    "Hot Dog": 1,
+    "Nachos w/ Cheese": 1,
+    "Cracker Jack": 0,
+    "Tenders & Fries": 0,
+    "M&M's": 0,
+    "M&M Peanut": 0,
+    "Peanuts": 6,
+    "Dicso Lemonade": 0
+  },
+  {
+    "Date": "04-18-2017",
+    "Total Sales (without service fee)": 43.5,
+    "Service Fees": 6,
+    "Total Orders": 3,
+    "Products Sold": 8,
+    "Water": 0,
+    "Coca-Cola": 0,
+    "Labatt Blue Light": 1,
+    "Labatt Blue": 0,
+    "Coors Light": 0,
+    "Blue Moon": 1,
+    "Labatt Blue Light Lime": 0,
+    "Diet Coke": 0,
+    "Root Beer": 0,
+    "Sprite": 0,
+    "Lemonade": 0,
+    "Pretzel": 2,
+    "Pretzel w/ Cheese": 0,
+    "Hot Dog": 1,
+    "Nachos w/ Cheese": 1,
+    "Cracker Jack": 0,
+    "Tenders & Fries": 1,
+    "M&M's": 0,
+    "M&M Peanut": 0,
+    "Peanuts": 1,
+    "Dicso Lemonade": 0
+  },
+  {
+    "Date": "04-21-2017",
+    "Total Sales (without service fee)": 51.25,
+    "Service Fees": 7.5,
+    "Total Orders": 5,
+    "Products Sold": 10,
+    "Water": 0,
+    "Coca-Cola": 0,
+    "Labatt Blue Light": 1,
+    "Labatt Blue": 1,
+    "Coors Light": 0,
+    "Blue Moon": 0,
+    "Labatt Blue Light Lime": 0,
+    "Diet Coke": 0,
+    "Root Beer": 0,
+    "Sprite": 1,
+    "Lemonade": 0,
+    "Pretzel": 0,
+    "Pretzel w/ Cheese": 0,
+    "Hot Dog": 4,
+    "Nachos w/ Cheese": 1,
+    "Cracker Jack": 0,
+    "Tenders & Fries": 1,
+    "M&M's": 1,
+    "M&M Peanut": 0,
+    "Peanuts": 0,
+    "Dicso Lemonade": 0
+  },
+  {
+    "Date": "04-23-2017",
+    "Total Sales (without service fee)": 93.75,
+    "Service Fees": 13.5,
+    "Total Orders": 6,
+    "Products Sold": 18,
+    "Water": 1,
+    "Coca-Cola": 1,
+    "Labatt Blue Light": 1,
+    "Labatt Blue": 2,
+    "Coors Light": 0,
+    "Blue Moon": 4,
+    "Labatt Blue Light Lime": 0,
+    "Diet Coke": 0,
+    "Root Beer": 1,
+    "Sprite": 0,
+    "Lemonade": 1,
+    "Pretzel": 0,
+    "Pretzel w/ Cheese": 2,
+    "Hot Dog": 2,
+    "Nachos w/ Cheese": 0,
+    "Cracker Jack": 0,
+    "Tenders & Fries": 1,
+    "M&M's": 0,
+    "M&M Peanut": 0,
+    "Peanuts": 2,
+    "Dicso Lemonade": 0
+  },
+  {
+    "Date": "05-03-2017",
+    "Total Sales (without service fee)": 4,
+    "Service Fees": 0.75,
+    "Total Orders": 1,
+    "Products Sold": 1,
+    "Water": 0,
+    "Coca-Cola": 0,
+    "Labatt Blue Light": 0,
+    "Labatt Blue": 0,
+    "Coors Light": 0,
+    "Blue Moon": 0,
+    "Labatt Blue Light Lime": 0,
+    "Diet Coke": 0,
+    "Root Beer": 0,
+    "Sprite": 0,
+    "Lemonade": 0,
+    "Pretzel": 0,
+    "Pretzel w/ Cheese": 0,
+    "Hot Dog": 0,
+    "Nachos w/ Cheese": 0,
+    "Cracker Jack": 0,
+    "Tenders & Fries": 0,
+    "M&M's": 0,
+    "M&M Peanut": 0,
+    "Peanuts": 1,
+    "Dicso Lemonade": 0
+  },
+  {
+    "Date": "05-10-2017",
+    "Total Sales (without service fee)": 104.5,
+    "Service Fees": 12.75,
+    "Total Orders": 7,
+    "Products Sold": 17,
+    "Water": 0,
+    "Coca-Cola": 2,
+    "Labatt Blue Light": 0,
+    "Labatt Blue": 0,
+    "Coors Light": 0,
+    "Blue Moon": 2,
+    "Labatt Blue Light Lime": 0,
+    "Diet Coke": 0,
+    "Root Beer": 0,
+    "Sprite": 0,
+    "Lemonade": 1,
+    "Pretzel": 1,
+    "Pretzel w/ Cheese": 1,
+    "Hot Dog": 2,
+    "Nachos w/ Cheese": 1,
+    "Cracker Jack": 0,
+    "Tenders & Fries": 6,
+    "M&M's": 1,
+    "M&M Peanut": 0,
+    "Peanuts": 0,
+    "Dicso Lemonade": 0
+  },
+  {"Date":"05-11-2017","Total Sales (without service fee)":24.75,"Service Fees":3.75,"Total Orders":2,"Products Sold":5,"Water":0,"Coca-Cola":0,"Labatt Blue Light":2,"Labatt Blue":0,"Coors Light":0,"Blue Moon":0,"Labatt Blue Light Lime":0,"Diet Coke":0,"Root Beer":0,"Sprite":0,"Lemonade":0,"Pretzel":0,"Pretzel w/ Cheese":0,"Hot Dog":3,"Nachos w/ Cheese":0,"Cracker Jack":0,"Tenders & Fries":0,"M&M's":0,"M&M Peanut":0,"Peanuts":0,"Dicso Lemonade":0}
+];
+
+var labelKeys = {
+  water:"Water",
+  cocaCola:"Coca-Cola",
+  labattBlueLight:"Labatt Blue Light",
+  labattBlue:"Labatt Blue",
+  coorsLight:"Coors Light",
+  blueMoon:"Blue Moon",
+  labattBlueLightLime:"Labatt Blue Light Lime",
+  dietCoke:"Diet Coke",
+  rootBeer:"Root Beer",
+  sprite:"Sprite",
+  lemonade:"Lemonade",
+  pretzel:"Pretzel",
+  pretzelCheese: "Pretzel w/ Cheese",
+  discoLemonade: "Dicso Lemonade",
+  hotDog:"Hot Dog",
+  nachosCheese:"Nachos w/ Cheese",
+  crackerJack:"Cracker Jack",
+  tendersFries:"Tenders & Fries",
+  mms:"M&M's",
+  mmPeanut:"M&M Peanut",
+  peanuts:"Peanuts"
+}
+
+$(function(){
+  $("#tabs").tabs();
+  updateTimeLine();
+  updateProductsBar();
+  updateDrillDown();
+});
+
+function getDbId(prop, value) {
+  for(var i = 0; i < db.length; i ++){
+    if(db[i][prop] === value){
+      return i;
+    }
+  }
+}
+
+function genProductInfo(start, limit) {
+  var sortedProds = [];
+  var product = {
+    water: 0,
+    cocaCola: 0,
+    labattBlueLight: 0,
+    labattBlue: 0,
+    coorsLight: 0,
+    blueMoon: 0,
+    labattBlueLightLime: 0,
+    dietCoke: 0,
+    rootBeer: 0,
+    sprite: 0,
+    lemonade: 0,
+    pretzel : 0,
+    hotDog: 0,
+    nachosCheese: 0,
+    crackerJack: 0,
+    tendersFries : 0,
+    mms: 0,
+    mmPeanut: 0,
+    peanuts: 0,
+    pretzelCheese: 0,
+    discoLemonade: 0
+  };
+  
+  for(i = start; i < limit; i++){
+    product.water+=db[i][labelKeys.water];
+    product.cocaCola+=db[i][labelKeys.cocaCola];
+    product.labattBlueLight+=db[i][labelKeys.labattBlueLight];
+    product.labattBlue+=db[i][labelKeys.labattBlue];
+    product.coorsLight+=db[i][labelKeys.coorsLight];
+    product.blueMoon+=db[i][labelKeys.blueMoon];
+    product.labattBlueLightLime+=db[i][labelKeys.labattBlueLightLime];
+    product.dietCoke+=db[i][labelKeys.dietCoke];
+    product.rootBeer+=db[i][labelKeys.rootBeer];
+    product.sprite+=db[i][labelKeys.sprite];
+    product.lemonade+=db[i][labelKeys.lemonade];
+    product.pretzel+=db[i][labelKeys.pretzel];
+    product.hotDog+=db[i][labelKeys.hotDog];
+    product.nachosCheese+=db[i][labelKeys.nachosCheese];
+    product.crackerJack+=db[i][labelKeys.crackerJack];
+    product.tendersFries+=db[i][labelKeys.tendersFries];
+    product.mms+=db[i][labelKeys.mms];
+    product.mmPeanut+=db[i][labelKeys.mmPeanut];
+    product.peanuts+=db[i][labelKeys.peanuts];
+    product.pretzelCheese+=db[i][labelKeys.pretzelCheese];
+    product.discoLemonade+=db[i][labelKeys.discoLemonade];
+  }
+  
+  for (var prod in product) {
+    sortedProds.push([prod, product[prod]]);
+  }
+  
+  sortedProds.sort(function(a, b) {
+      return b[1] - a[1];
+  });
+  
+  return sortedProds;
+}
+
+function formatMoney(n, currency) {
+  return currency + "" + n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+}
+
+function updateTimeLine(){
+    var chartLbls = [];
+    var chartData = [];
+    var ctxLine = $("#timeLineChart");
+    var totalSalesWithOutFees = 0;
+    var orderCount = 0;
+  
+    for(var i = 0; i < db.length; i++){
+        chartLbls.push(db[i]["Date"]);
+        chartData.push(db[i]["Total Sales (without service fee)"]);
+        totalSalesWithOutFees+=db[i]["Total Sales (without service fee)"];
+        orderCount+=db[i]["Total Orders"];
+    }
+    
+    //for aesthetics..make it look like we are connectig to an actual db
+    setTimeout(function(){ 
+      $("#totalSalesNoFees").html(formatMoney(totalSalesWithOutFees,"$"));
+      $("#totalOrders").html(orderCount);
+    }, 1000);
+  
+    var timeLineChart = new Chart(ctxLine,{
+        type: 'line',
+        data: {
+        labels: chartLbls,
+        datasets: [
+            	{
+                	label: "Sales w/out Service Fees ($)",
+                	data: chartData,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    lineTension: 0.1
+            	}
+        	]
+    	},
+		options: {
+			responsive: true,
+			maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+            {
+                ticks: {
+                    beginAtZero: true
+                },
+                scaleLabel: {
+                    display: true
+                }
+            }
+        ]
+      }
+    }
+   });
+}
+
+function updateProductsBar(){
+  var lbls = [];
+  var dta = [];
+  var bgColors = [];
+  var borderColors = [];
+  var sortedProds = genProductInfo(0,db.length);
+  var prodSum = 0;
+  var ctxBar = $("#productsBarChart");
+  
+  for(var i = 0; i < sortedProds.length; i++){
+    if(sortedProds[i][1] !== 0){
+      lbls.push(labelKeys[sortedProds[i][0]]);
+      dta.push(sortedProds[i][1]);
+      prodSum += sortedProds[i][1];
+      bgColors.push('rgba(54, 162, 235, 0.2)');
+      borderColors.push('rgba(54, 162, 235, 1)');
+    }
+  }
+  
+  //for aesthetics..make it look like we are connectig to an actual db
+  setTimeout(function(){ 
+      $("#totalProductsSold").html(prodSum);
+  }, 1000);
+  
+  var data = {
+    labels: lbls,
+    datasets: [
+        {
+            label: "Count",
+            backgroundColor: bgColors,
+            borderColor: borderColors,
+            borderWidth: 1,
+            data: dta,
+        }
+    ]
+  };
+  
+    new Chart(ctxBar, {
+      type: "horizontalBar",
+      data: data,
+      options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            animation: false,
+            title: {
+                display: true,
+                // text: titleText
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        // max: data.labels[limit],
+                        fontSize: 12
+                    }
+                }]
+            }
+        }   
+  });
+  
+}
+
+function updateDrillDown() {
+    // $('#table').html("")
+     $('#table').bootstrapTable({
+        columns: [{
+            field: 'Date',
+            title: 'Date',
+            sortable: true
+        }, {
+            field: 'Total Sales (without service fee)',
+            title: 'Sales w/out Fees',
+            sortable: true,
+            formatter: function (value){
+              return "$" + "" + value.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+            }
+        }, {
+            field: 'Total Orders',
+            title: 'Orders',
+            sortable: true
+        }, {
+            field: 'Products Sold',
+            title: 'Products Sold',
+            sortable: true
+        }],
+        data: db,
+        onExpandRow: function (index, row, $detail) {
+            expandTable(getDbId("Date", row.Date),row,$detail);
+        },
+        pagination: true,
+        search: true,
+        sortName: 'Date', 
+        sortOrder: 'desc'
+    });
+}
+
+function expandTable(index,row,$detail) {
+  $detail.html('<ul id = "ul_'+index+'" class="detailsUl"></ul>');
+ 
+  var correctProds = genProductInfo(index,index + 1);
+  
+  for(var i = 0; i < correctProds.length; i++){
+    if(correctProds[i][1]!== 0){
+      $("#ul_"+index).append('<li>' + labelKeys[correctProds[i][0]] + ': ' + correctProds[i][1] + '</li>');
+    }
+  }
+  
+}
